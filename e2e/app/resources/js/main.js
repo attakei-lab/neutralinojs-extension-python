1305d2fc53d8a949223b45346ac57228cfad5204
Kazuya Takei <myself@attakei.net>
2025-11-23 05:26:31 +0900

    ci(e2e): Support custom behavior
    - note: Currently parallel running is not supported

--- e2e/app/resources/js/main.js
+++ e2e/app/resources/js/main.js
@@ -17,16 +17,25 @@ Neutralino.events.on("app_updateTitle", (e) => {
   const pidPath = await Neutralino.filesystem.getJoinedPath(tmpDir, "pid.txt");
   await Neutralino.filesystem.writeFile(pidPath, NL_APPID);
   const watcherId = await Neutralino.filesystem.createWatcher(tmpDir);
-  Neutralino.events.on("watchFile", (event) => {
+  Neutralino.events.on("watchFile", async (event) => {
     console.debug("File changed:", event.detail);
     if (watcherId !== event.detail.id) return;
     const { dir, filename, action } = event.detail;
     if (dir === tmpDir && filename === "pid.txt" && action === "delete") {
       Neutralino.app.exit();
     }
+    if (dir === tmpDir && filename === "command.js" && action === "add") {
+      eval(
+        await Neutralino.filesystem.readFile(
+          await Neutralino.filesystem.getJoinedPath(tmpDir, "command.js"),
+        ),
+      );
+      await Neutralino.filesystem.remove(
+        await Neutralino.filesystem.getJoinedPath(tmpDir, "command.js"),
+      );
+    }
   });
 })();
-
 // Start the application
 Neutralino.extensions.dispatch(
   "dev.attakei.neutralinojs.pythonext.e2e.backend",
